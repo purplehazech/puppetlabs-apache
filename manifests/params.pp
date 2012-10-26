5d94d797b49a2f2f08a07e185e5e867a6de30239
Lucas Bickel <hairmare@purplehaze.ch>
2012-10-26 12:29:20 +0200

    replace osfamily with operatingsysytem pending puppet release
    feature request ist here: http://projects.puppetlabs.com/issues/17029

--- manifests/params.pp
+++ manifests/params.pp
@@ -83,7 +83,7 @@ class apache::params {
     }
     $mod_libs              = {}
     $mod_identifiers       = {}
-  } elsif $::osfamily == 'gentoo' {
+  } elsif $::operatingsystem == 'gentoo' {
     $user                  = 'apache'
     $group                 = 'apache'
     $apache_name           = 'apache2'
