d95fef874cfb07ae887f04312ab02b10ac848dcf
Lucas Bickel <hairmare@purplehaze.ch>
2012-10-30 23:29:56 +0100

    apache includefile for rewriting

---
+++ manifests/vhost/include/rewrite.pp
@@ -0,0 +1,18 @@
+# Define: apache::vhost::include::rewrite
+#
+#
+define apache::vhost::include::rewrite (
+    $vhost_name,
+    $rules,
+    $priority = 10,
+    $template = 'apache/vhost-include-rewrite.include.erb'
+) {
+    file { "${vhost_name}-${priority}-${name}.include":
+        content => template($template),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Package['httpd'],
+        notify  => Service['httpd'],
+    }
+}
