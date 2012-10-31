0fe6abf4e6d4a45d48c92904fb808581934e48fb
Lucas Bickel <hairmare@purplehaze.ch>
2012-10-31 19:43:17 +0100

    add path to file resource

--- manifests/vhost/include/rewrite.pp
+++ manifests/vhost/include/rewrite.pp
@@ -8,6 +8,7 @@ define apache::vhost::include::rewrite (
     $template = 'apache/vhost-include-rewrite.include.erb'
 ) {
     file { "${vhost_name}-${priority}-${name}.include":
+        path   => "${apache::params::vdir}/${vhost_name}-${priority}-${name}.include",
         content => template($template),
         owner   => 'root',
         group   => 'root',
